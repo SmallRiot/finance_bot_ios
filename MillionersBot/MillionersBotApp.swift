58083fc2919937f7032ef83f29e0649a95dc519b
Riot <danila10fedin@gmail.com>
2026-05-29 18:30:12 +0400

    Initial Commit

---
+++ MillionersBot/MillionersBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionersBotApp.swift
+//  MillionersBot
+//
+//  Created by danila on 29.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MillionersBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
